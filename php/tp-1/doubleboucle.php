706d4dee97d297015fb7775b905b1db41cf56861
ad-panda <108462406+ad-panda@users.noreply.github.com>
2025-11-17 16:12:23 +0100

    Update doubleboucle.php

--- php/tp-1/doubleboucle.php
+++ php/tp-1/doubleboucle.php
@@ -5,6 +5,12 @@
     <title>doubleboucle</title>
 </head>
 <body>
+<form method="post">
+        <label for="nbr">nombre :</label>
+        <input type="int" id="nbr" name="nbr" required>
+        <br><br>
+        <button type="submit">Envoyer</button>
+    </form>
     <?php
     function doubleboucle($nbr){
       $i = 0;
@@ -22,7 +28,7 @@
       return $liste;
     } 
 
-      $nbr = 5;
+      $nbr = (int)($_POST["nbr"]);
       $liste=doubleboucle($nbr);
       echo "<p>affiche pour $nbr <br>" ;
       echo"$liste<br>";

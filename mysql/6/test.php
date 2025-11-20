9d8a6d65ba421ea5b9c221e2b9358a5e0f4851de
ad-panda <108462406+ad-panda@users.noreply.github.com>
2025-11-20 12:01:08 +0100

    Update test.php

--- mysql/6/test.php
+++ mysql/6/test.php
@@ -1,6 +1,19 @@
 <?php
 
+session_start();
+
+$_SESSION["email"] = "john@gmail.com";
+
+if (!isset($_SESSION["counter"]))
+{
+  $_SESSION["counter"] = 0;
+}
+
+$_SESSION["counter"]++;
+
 require_once "config.php";
+
+//creer variable et verifie si variable definie par l'url a l'ouverture du fichier et donne la bonne valeur a la variable
 
 $sort = "nom";
 
@@ -8,11 +21,13 @@ if (isset($_GET["sort"])) {
   $sort = $_GET["sort"];
 }
 
-$order = "desc";
+$order = "asc";
 
 if (isset($_GET["order"])) {
  $order = $_GET["order"];
 }
+
+//permet de verifier que l'url correspond a des variables existant dans le mysql
 
 if(!in_array($sort, ["nom","pays","course","temps"])) {
   die("Invalid sort");
@@ -22,10 +37,36 @@ if(!in_array($order, ["asc","desc"])) {
   die("Invalid order");
 }
 
+//verification method et si username existe deja
+
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  $_SESSION["username"] = $_POST["username"];
+}
+
+
+if (isset($_SESSION["username"]))
+{
+  echo '
+  <h1>l\'utilisateur '.$_SESSION["username"].' est connecter</h1>
+  ';
+} else {
+  echo '
+  <form action="test.php" method="POST">
+<h1>Login</h1>
+<label>Username : </label>
+<input type="text" id="username" name="username">
+</form>
+  ';
+}
+
+
 $sth=$dbh->prepare("SELECT * FROM jo.`100` order by ".$sort." ".$order);
 $sth->execute();
 
 $data = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<h1>".$_SESSION["counter"]."</h1>";
 
 echo"<table>
 <thead>
